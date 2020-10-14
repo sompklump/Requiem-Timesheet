d175326bce664cb2eca77e7d396dc1a0f260e78f
Klump <oleanton03@hotmail.com>
2020-10-14 09:48:28 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html style="background:#212121;">
+  <head>
+    <title>Off</title>
+  </head>
+  <body>
+  <div class="container">
+    <audio controls autoplay hidden="true">
+      <source src="assets/sounds/trolled.ogg" type="audio/ogg">
+      <source src="assets/sounds/trolled.mp3" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+</body>
+</html>
